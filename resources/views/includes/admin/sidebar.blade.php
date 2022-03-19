861e019d78cda2d103ad55d7cd9c6bfd1660bc02
vincentmanono <vincentnyamongo95@gmail.com>
2022-03-19 14:02:46 +0300

    merge changes

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('home')}}" class="brand-link">
-        <img src="{{asset('/storage/profile/'. auth()->user()->image)}}" alt="AdminLTE Logo"
+        <img src="{{asset('/storage/profile/'. auth()->user()->image)}}" alt=""
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">{{config('app.name')}}</span>
     </a>
